f9d7078bf6844f8339d95701d3f0b742dd620883
joyce <jhan79@calpoly.edu>
2026-05-03 22:54:41 -0700

    Task 1

--- proj2.py
+++ proj2.py
@@ -5,9 +5,25 @@ from typing import *
 
 
 # Put your data definitions first!
-
+@dataclass(frozen=True)
+class Row:
+    country: str
+    year: int
+    electricity_and_heat_co2_emissions: Optional[float]
+    electricity_and_heat_co2_emissions_per_capita: Optional[float]
+    energy_co2_emissions: Optional[float]
+    energy_co2_emissions_per_capita: Optional[float]
+    total_co2_emissions_excluding_lucf: Optional[float]
+    total_co2_emissions_excluding_lucf_per_capita: Optional[float]
+@dataclass(frozen=True)
+class Node:
+    value: Row
+    next: Optional[Node]
 # ...
 
 # Then your functions.
-
+def parse_float(value: str) -> Optional[float]:
+    if value == "":
+        return None
+    return float(value)
 # ...
